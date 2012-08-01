02740fcd83de3e890ac1e279c364f2b05f13c248
Akash Badshah <akashbad4123@gmail.com>
2012-08-01 15:42:07 +0200

    All gestures appear to be working, THRESHOLDS needed for appropriate response

--- Gesture.cpp
+++ Gesture.cpp
@@ -69,9 +69,9 @@ String Gesture::process(GestureHandler *pHandler, sensorData data)
 	results+= handler.getRotating();
 	results+= handler.getFalling();
 	results+= handler.getUpsideDown();
-	//	results+= handler.getTorso();
-	//	results+= handler.getBottom();
-	//	results+= handler.getStomach();
+	results+= handler.getTorso();
+	results+= handler.getBottom();
+	results+= handler.getStomach();
 	results+= handler.getTailTouch();
 	results+= handler.getKiss();
 	handler.update();
